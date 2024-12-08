db686594811eb48dcd9aa13e842458d8faa4df95
xiaoka <panjixiao@outlook.com>
2024-12-08 11:43:12 +0900

    understanding event driven code execution

--- app.js
+++ app.js
@@ -20,15 +20,14 @@ const server = http.createServer((req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
       fs.writeFileSync("message.txt", message);
+      res.statusCode = 302; //redirection
+      res.setHeader("Location", "/");
+      return res.end();
     });
-
-    res.statusCode = 302; //redirection
-    res.setHeader("Location", "/");
-    return res.end();
   }
   //   process.exit();
   res.setHeader("Content-Type", "text/html");
