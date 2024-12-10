74cf7a5dcf5251311fb2d96f4822bcde43c5f968
Carlos <156889987+msd-pan@users.noreply.github.com>
2024-12-10 10:17:50 +0900

    Serving HTML pages and Returning a 404 page

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 // /admin/add-product=> GET
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/admin/product' method='post'><input type='text' name='title'><button type='submit'>Add Product</form>"
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 // /admin/product =>POST
