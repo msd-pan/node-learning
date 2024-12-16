0282923471ec487d32728db8a7ec5d48253c1304
Carlos <156889987+msd-pan@users.noreply.github.com>
2024-12-16 15:42:44 +0900

    Working with EJS

--- app.js
+++ app.js
@@ -2,19 +2,10 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const { engine } = require("express-handlebars");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  engine({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
