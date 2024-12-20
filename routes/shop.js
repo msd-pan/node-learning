4baf1af01054c06236f3e5f2bc9138fa749ecb7d
Carlos <156889987+msd-pan@users.noreply.github.com>
2024-12-20 13:30:00 +0900

    Extracting dynamic params

--- routes/shop.js
+++ routes/shop.js
@@ -10,6 +10,10 @@ router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
 
+router.get("/products/:productId", shopController.getProduct);
+// 动态路由，匹配 /products/ 后接任何动态参数的路径, 其必须是放在最后，不然之前的任意/products/都不会达到
+// Dynamic route, matches any route starting with /products/ followed by a dynamic parameter
+
 router.get("/cart", shopController.getCart);
 
 router.get("/orders", shopController.getOrders);
