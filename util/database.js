c54cc14d7f0ddc9151c342344153f6c70962d6a0
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-01-06 17:28:38 +0900

    CHP11: understanding sequelize => connecting to the database

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "msd123456", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "msd123456",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
