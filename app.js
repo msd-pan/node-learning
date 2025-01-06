a2c5a04687b29cb272034333a9d630c521e4cd1f
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-01-06 16:13:38 +0900

    Fetching products

--- app.js
+++ app.js
@@ -30,14 +30,6 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-db.execute("select * from products")
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
 // 使用 body-parser 解析表单数据
 // Use body-parser to parse incoming form data
 app.use(bodyParser.urlencoded({ extended: false }));
