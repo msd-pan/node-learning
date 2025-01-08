776b777f9d20a0fe87bff6af9cc41706f916c903
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-01-08 10:55:19 +0900

    Inserting data & creating a product

--- util/database.js
+++ util/database.js
@@ -1,4 +1,5 @@
-const Sequelize = require("sequelize");
+const { Sequelize } = require("sequelize");
+// use { Sequelize } instead of Sequelize to Activate NodeJS Sequelize Intellisense VS Code
 
 const sequelize = new Sequelize("node-complete", "root", "msd123456", {
   dialect: "mysql",
