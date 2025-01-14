33d45260b0f84d0a04d225764a1707851f7c8027
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-01-14 17:01:46 +0900

    One-to-many & Many-to-one relations

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 // 创建Express应用实例
 // Create an Express application instance
@@ -63,7 +65,11 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
+User.hasMany(Product); // (optional) a one-direction-difination is enough
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 const startServer = async () => {
   try {
