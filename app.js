8383071ac161aaf3ed6f6c9b4adcaf75a7eba728
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-01-15 16:06:49 +0900

    Adding an order model

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 // 创建Express应用实例
 // Create an Express application instance
@@ -70,6 +72,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 const startServer = async () => {
   try {
