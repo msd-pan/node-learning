883b1d0edff2f181fe57ec8c7ff8159cd84fc795
xiaoka <panjixiao@outlook.com>
2025-01-31 20:40:29 +0900

    Working on cart items&orders

--- models/user.js
+++ models/user.js
@@ -3,10 +3,13 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 const ObjectId = mongodb.ObjectId;
+
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.username = username;
     this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
   }
 
   async save() {
@@ -14,6 +17,25 @@ class User {
       const db = getDb();
 
       const result = await db.collection("users").insertOne(this);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async addToCart(product) {
+    try {
+      // const cartProduct = this.cart.items.findIndex((cp) => {
+      //   return cp._id === product._id;
+      // });
+
+      const updatedCart = { items: [{ ...product, quantity: 1 }] };
+      const db = getDb();
+      await db
+        .collection("users")
+        .updateOne(
+          { _id: new ObjectId(this._id) },
+          { $set: { cart: updatedCart } }
+        );
     } catch (err) {
       console.log(err);
     }
