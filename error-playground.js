d7de6b18ae11433ebfaa5dbbb78d6a41289759bc
xiaoka <panjixiao@outlook.com>
2025-02-10 16:11:38 +0900

    Errors - some theory

---
+++ error-playground.js
@@ -0,0 +1,16 @@
+const sum = (a, b) => {
+  if (a && b) {
+    return a + b;
+  }
+  throw new Error("Invalid arguments");
+};
+
+try {
+  console.log(sum(1));
+} catch (err) {
+  console.log("Error occured !");
+  console.log(err);
+}
+
+// console.log(sum(1));
+console.log("this works!");
