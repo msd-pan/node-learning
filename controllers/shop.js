9ed05c0f945d3e3887bfdb0c6599efbe89876d00
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-12 16:05:51 +0900

    Streaming data VS preloading data

--- controllers/shop.js
+++ controllers/shop.js
@@ -145,13 +145,19 @@ exports.getInvoice = async (req, res, next) => {
 
     const invoiceName = "invoice-" + orderId + ".pdf";
     const invoicePath = path.join("data", "invoices", invoiceName);
-    fs.readFile(invoicePath, (err, data) => {
-      if (err) return next(err);
+    // fs.readFile(invoicePath, (err, data) => {
+    //   if (err) return next(err);
 
-      res.setHeader("Content-type", "application/pdf");
-      res.setHeader("Content-Disposition", "inline; filename=" + invoiceName);
-      res.send(data);
-    });
+    //   res.setHeader("Content-type", "application/pdf");
+    //   res.setHeader("Content-Disposition", "inline; filename=" + invoiceName);
+    //   res.send(data);
+    // });
+
+    const file = fs.createReadStream(invoicePath);
+    res.setHeader("Content-type", "application/pdf");
+    res.setHeader("Content-Disposition", "inline; filename=" + invoiceName);
+
+    file.pipe(res);
   } catch (err) {
     next(err);
   }
