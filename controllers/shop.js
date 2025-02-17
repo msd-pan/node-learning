c0ffb8fbd2b5085347208839f6b53137f9001467
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-17 13:25:24 +0900

    Adding a checkout page

--- controllers/shop.js
+++ controllers/shop.js
@@ -99,6 +99,29 @@ exports.getCart = async (req, res, next) => {
   }
 };
 
+exports.getCheckout = async (req, res, next) => {
+  try {
+    const user = await req.user.populate("cart.items.productId");
+    const products = user.cart.items;
+
+    let total = 0;
+    products.forEach((p) => {
+      total += p.quantity * p.productId.price;
+    });
+
+    res.render("shop/checkout", {
+      path: "/checkout",
+      pageTitle: "Checkout",
+      products: products,
+      totalSum: total,
+    });
+  } catch (err) {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
+  }
+};
+
 exports.postCart = async (req, res, next) => {
   const prodId = req.body.productId;
   try {
