cd8b845f5943b413bd25a593e30c1556db649105
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-17 16:47:25 +0900

    Sending requests & responses and working with postman

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,22 @@
-exports.getPosts = async (req, res, next) => {};
+exports.getPosts = async (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: "first product", content: "this is the first post!" }],
+  });
+};
+
+exports.postPost = async (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  //   create post in db
+  res.status(201).json({
+    message: "Post created successfully!",
+    posts: [
+      {
+        id: new Date().toString(),
+        title,
+        content,
+      },
+    ],
+  });
+};
