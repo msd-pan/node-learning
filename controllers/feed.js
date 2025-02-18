28fd64f12844b15aa611448f886f8e22677fbc03
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-18 15:50:00 +0900

    Updating posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
@@ -70,4 +73,54 @@ exports.getPost = async (req, res, next) => {
     }
     next(err);
   }
+};
+
+exports.updatePost = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error("Validation failed, entered data is incorrect.");
+      error.statusCode = 422;
+      throw error;
+    }
+    const { postId } = req.params;
+    const { title, content } = req.body;
+    let imageUrl = req.body.image;
+    if (req.file) {
+      imageUrl = req.file.path.replace("\\", "/");
+    }
+    if (!imageUrl) {
+      const error = new Error("No file picked");
+      error.statusCode = 422;
+      throw error;
+    }
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      const error = new Error("Could not find post.");
+      error.statusCode = 404;
+      throw error;
+    }
+    if (imageUrl !== post.imageUrl) {
+      clearImage(post.imageUrl);
+    }
+    post.title = title;
+    post.imageUrl = imageUrl;
+    post.content = content;
+    const result = await post.save();
+
+    res.status(200).json({ message: "Post updated", post: result });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => {
+    console.log(err);
+  });
 };
