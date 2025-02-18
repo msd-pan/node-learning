2b6cbfb888e0b5d15cc13f6133d09610f7440ab0
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-18 11:15:28 +0900

    Static images & error handling

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -12,6 +14,7 @@ const MONGODB_URI =
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 
 app.use(bodyParser.json()); // application/json
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -24,6 +27,13 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message });
+});
 
 const startServer = async () => {
   try {
--- controllers/feed.js
+++ controllers/feed.js
@@ -23,10 +23,9 @@ exports.createPost = async (req, res, next) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(422).json({
-        message: "Validation failed, entered data is incorrect.",
-        errors: errors.array(),
-      });
+      const error = new Error("Validation failed, entered data is incorrect.");
+      error.statusCode = 422;
+      throw error;
     }
 
     const { title, content } = req.body;
@@ -47,6 +46,9 @@ exports.createPost = async (req, res, next) => {
       post: result,
     });
   } catch (err) {
-    console.log(err);
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
   }
 };
