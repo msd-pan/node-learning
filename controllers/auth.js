c3812b004293f0f77e4f4553d6f9ed430a49bc22
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-19 11:02:55 +0900

    Time to practice - Working with REST APIs

--- controllers/auth.js
+++ controllers/auth.js
@@ -59,3 +59,44 @@ exports.login = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.getUserStatus = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      const error = new Error("A user could not be found.");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({ status: user.status });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.updateUserStatus = async (req, res, next) => {
+  try {
+    const { status } = req.body;
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      const error = new Error("A user could not be found.");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    user.status = status;
+    await user.save();
+    res.status(200).json({ message: "status updated successfully" });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
