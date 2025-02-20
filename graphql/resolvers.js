da173e11cf8644aca696773f6165b50eb4db5931
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-20 10:49:00 +0900

    Adding a login query & a resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,9 @@
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const bcryptjs = require("bcryptjs");
 
 module.exports = {
   createUser: async ({ userInput }, req) => {
@@ -39,5 +41,27 @@ module.exports = {
     const createdUser = await user.save();
 
     return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
+
+  login: async ({ email, password }, req) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+      const error = new Error("User not found.");
+      error.code = 401;
+      throw error;
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error("Password is incorrect.");
+      error.code = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      { userId: user._id.toString(), emial: user.email },
+      "somesupersecretsecret",
+      { expiresIn: "1h" }
+    );
+
+    return { token, userId: user._id.toString() };
   },
 };
