8b225cab6b7d2648df2cec1f3d8d6f5989028144
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-20 17:18:00 +0900

    Deleting posts

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const { graphqlHTTP } = require("express-graphql");
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
 const auth = require("./middleware/auth");
+const { clearImage } = require("./util/file");
 
 const app = express();
 
@@ -111,10 +112,3 @@ const startServer = async () => {
 };
 
 startServer();
-
-const clearImage = (filePath) => {
-  filePath = path.join(__dirname, "..", filePath);
-  fs.unlink(filePath, (err) => {
-    console.log(err);
-  });
-};
