0876e819a80934692b38316639fc85a221517d7a
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-20 10:27:29 +0900

    Connecting the frontend to the graphql api

--- app.js
+++ app.js
@@ -51,6 +51,9 @@ app.use((req, res, next) => {
     "GET, POST, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
 
@@ -60,7 +63,7 @@ app.use(
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
-    formatError(err) {
+    customFormatErrorFn(err) {
       if (!err.originalError) {
         return err;
       }
