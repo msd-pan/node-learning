d7c4a3a794d8dfde40417464b5a942cfe08feeb1
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-21 10:33:24 +0900

    Managing the user status

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -250,4 +250,37 @@ module.exports = {
 
     return true;
   },
+
+  user: async (args, req) => {
+    if (!req.isAuth) {
+      const error = new Error("Not authenticated!");
+      error.code = 401;
+      throw error;
+    }
+    const user = await User.findById(req.userId);
+    if (!user) {
+      const error = new Error("User not found.");
+      error.code = 404;
+      throw error;
+    }
+
+    return { ...user._doc, _id: user._id.toString() };
+  },
+
+  updateStatus: async ({ status }, req) => {
+    if (!req.isAuth) {
+      const error = new Error("Not authenticated!");
+      error.code = 401;
+      throw error;
+    }
+    const user = await User.findById(req.userId);
+    if (!user) {
+      const error = new Error("User not found.");
+      error.code = 404;
+      throw error;
+    }
+    user.status = status;
+    await user.save();
+    return { ...user._doc, _id: user._id.toString() };
+  },
 };
