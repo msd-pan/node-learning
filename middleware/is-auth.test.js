7fa447c4192b53534c82bf1bb10350bd7f5c3ff2
xiaoka <panjixiao@outlook.com>
2025-02-24 18:43:10 +0900

    Organizing multiple tests

--- middleware/is-auth.test.js
+++ middleware/is-auth.test.js
@@ -1,14 +1,28 @@
 // import authMiddleware from "./is-auth";
 const authMiddleware = require("../middleware/is-auth");
 
-test("should throw an error if no authorization header is present", () => {
-  const req = {
-    get: (headerName) => {
-      return null;
-    },
-  };
+describe("Auth middleware", () => {
+  test("should throw an error if no authorization header is present", () => {
+    const req = {
+      get: (headerName) => {
+        return null;
+      },
+    };
 
-  expect(() => {
-    authMiddleware(req, {}, () => {});
-  }).toThrow("Not authenticated.");
+    expect(() => {
+      authMiddleware(req, {}, () => {});
+    }).toThrow("Not authenticated.");
+  });
+
+  test("shoule throw an error if the authorization header is only one string", () => {
+    const req = {
+      get: (headerName) => {
+        return "xyz";
+      },
+    };
+
+    expect(() => {
+      authMiddleware(req, {}, () => {}).toThrow();
+    });
+  });
 });
