d3545495f36ec8bf3ae14b3350e1908ae724efa0
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-25 15:24:47 +0900

    Type inference & Type casting

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-var add = function (num1, num2) {
-  return num1 + num2;
-};
+var num1Element = document.getElementById("num1");
+var num2Element = document.getElementById("num2");
+var buttonElement = document.querySelector("button");
+var add = function (num1, num2) { return num1 + num2; };
+buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener("click", function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    var result = add(+num1, +num2);
+    console.log(result);
+});
 console.log(add(1, 6));
