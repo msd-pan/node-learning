a11346d9dcbfc99ce9963e62cb2f8cccc4754592
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-25 10:21:14 +0900

    Testing asynchronous code(with jest)

--- controllers/auth.test.js
+++ controllers/auth.test.js
@@ -2,11 +2,24 @@ const AuthController = require("../controllers/auth");
 const User = require("../models/user");
 
 describe("Auth controller - Login", () => {
-  test("should throw an error with code 500 if accessing the database fails", () => {
-    jest.spyOn(User, "findOne");
-    User.findOne.throws();
+  test("should throw an error with code 500 if accessing the database fails", async () => {
+    jest.spyOn(User, "findOne").mockImplementation(() => {
+      throw new Error("Database error");
+    });
 
-    expect(AuthController.login);
+    const req = {
+      body: {
+        email: "test@test.com",
+        password: "tester",
+      },
+    };
+
+    try {
+      await AuthController.login(req, {}, () => {});
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error); // 确保是 Error 实例
+      expect(error).toHaveProperty("statusCode", 500);
+    }
 
     User.findOne.mockRestore();
   });
