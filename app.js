7ee3556c48bc9e633281a8b3a5fa9c27baf482d0
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-25 15:31:39 +0900

    Configuring Typescript

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var num1Element = document.getElementById("num1");
-var num2Element = document.getElementById("num2");
-var buttonElement = document.querySelector("button");
-var add = function (num1, num2) { return num1 + num2; };
-buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener("click", function () {
-    var num1 = num1Element.value;
-    var num2 = num2Element.value;
-    var result = add(+num1, +num2);
+"use strict";
+const num1Element = document.getElementById("num1");
+const num2Element = document.getElementById("num2");
+const buttonElement = document.querySelector("button");
+const add = (num1, num2) => num1 + num2;
+buttonElement.addEventListener("click", () => {
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+    const result = add(+num1, +num2);
     console.log(result);
 });
 console.log(add(1, 6));
--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
 const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
-const buttonElement = document.querySelector("button");
+const buttonElement = document.querySelector("button")!;
 
 const add = (num1: number, num2: number) => num1 + num2;
 
-buttonElement?.addEventListener("click", () => {
+buttonElement.addEventListener("click", () => {
   const num1 = num1Element.value;
   const num2 = num2Element.value;
   const result = add(+num1, +num2);
