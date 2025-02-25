cda61e55e3a1349fda816d5f57b027ca0dc58ba8
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-25 15:39:12 +0900

    Working with union types

--- app.js
+++ app.js
@@ -2,11 +2,20 @@
 const num1Element = document.getElementById("num1");
 const num2Element = document.getElementById("num2");
 const buttonElement = document.querySelector("button");
-const add = (num1, num2) => num1 + num2;
+const add = (num1, num2) => {
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2;
+    }
+    else if (typeof num1 === "string" && typeof num2 === "string") {
+        return num1 + " " + num2;
+    }
+    return +num1 + +num2;
+};
 buttonElement.addEventListener("click", () => {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     const result = add(+num1, +num2);
     console.log(result);
+    const stringResult = add(num1, num2);
+    console.log(stringResult);
 });
-console.log(add(1, 6));
--- app.ts
+++ app.ts
@@ -2,13 +2,20 @@ const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const buttonElement = document.querySelector("button")!;
 
-const add = (num1: number, num2: number) => num1 + num2;
+const add = (num1: number | string, num2: number | string) => {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else if (typeof num1 === "string" && typeof num2 === "string") {
+    return num1 + " " + num2;
+  }
+  return +num1 + +num2;
+};
 
 buttonElement.addEventListener("click", () => {
   const num1 = num1Element.value;
   const num2 = num2Element.value;
   const result = add(+num1, +num2);
   console.log(result);
+  const stringResult = add(num1, num2);
+  console.log(stringResult);
 });
-
-console.log(add(1, 6));
