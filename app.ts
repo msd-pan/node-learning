f6f1590d8b1bd835d44ebc69fb2bfbb46ea6ac29
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-26 09:36:16 +0900

    Adding REST routes with typescript

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 // const express = require("express");
+import bodyParser from "body-parser";
 
 import todosRoutes from "./routes/todos";
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(todosRoutes);
 
