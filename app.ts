34ae35233f67aef3b761c2a21d7cd36e4b0a0d73
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-02-26 09:23:51 +0900

    Writing typescript express.js code

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
 import express from "express";
 // const express = require("express");
 
+import todosRoutes from "./routes/todos";
+
 const app = express();
+
+app.use(todosRoutes);
 
 app.listen(3000);
